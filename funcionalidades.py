11a36d3c25629b695cc4d9ae8fd6bad98303b5f6
Davisson-Adriel <droman819@unab.edu.co>
2025-04-03 06:45:28 -0500

    feat: :sparkles: Agregar Paquete
    Se agrega la funcionalidad de crear un paquete nuevo

--- funcionalidades.py
+++ funcionalidades.py
@@ -1,8 +1,27 @@
-def mostrarmenu():
-    print("""
-          
----------------------- Bienvenido ----------------------
-1. FUn1
-          2. FUncio 2
-          """)
+paquetes={}
+
+def agpaquete():
     
+    while True:
+
+        nom=input("Escriba el nombre del paquete: ")
+        eva=paquetes.get(nom,1)
+
+        if eva!=1:
+            break
+        else:
+            print("Paquete ya existente")
+        
+    while True:
+
+        prec=input("Ingrese el precio del paquete: ")
+        if prec.isdigit():
+            break
+        else:
+            print("El dato ingresado no corresponde a un valor numerico")
+
+    tip=input("Ingrese el tipo de evento del paquete: ")
+    dura=input("Ingrese la duración del paquete: ")
+
+    paquetes[nom]={"Nombre":nom,"Precio":prec,"Tipo":tip,"Duración":dura}
+
