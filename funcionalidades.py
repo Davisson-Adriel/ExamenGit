72d240fcf86192c862f03f775872e58d2da417b6
Davisson-Adriel <droman819@unab.edu.co>
2025-04-03 07:17:05 -0500

    feat: :sparkles: Eliminar paquete
    Se agrega la función de eliminar paquetes ya no ofrecidos

--- funcionalidades.py
+++ funcionalidades.py
@@ -60,5 +60,38 @@ def agservicio():
     servicios[cod]={"Nombre del Titular":nombre,"Paquete":noms}
     print("SERVICIO REGISTRADO CON EXITO")
 
+def deletpaquete():
 
+    while True:
 
+            noms=input("Ingrese el nombre del paquete que desea eliminar:")
+            noms=noms.capitalize()
+            eva=paquetes.get(noms,1)
+            if eva!=1:
+                print("Paquete no existente")
+            else:
+                break
+    print("------------------------")
+    for j in paquetes[noms]:
+        print(j,"-",paquetes[noms][j])
+    print("------------------------")
+        
+    while True:
+        try:
+            print("----------------------------------------------")      
+            print("¿Esta seguro que desea eliminar este paquete?")
+            print("""
+                1. SI
+                2. NO
+                    """)
+            opc=int(input("Ingrese el numero de acuerdo a su elección"))
+            if opc==1:
+                print("Paquete eliminado con exito")
+                paquetes.pop(noms)
+                break
+            elif opc==2:
+                break
+            else:
+                print("Valor no valido")
+        except ValueError:
+            print("Valor no valido")
